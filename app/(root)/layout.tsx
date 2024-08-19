2e70738963e8712f6a1e2e87f6436360e32b9a57
Fernando Brino <fernandobrino11@gmail.com>
2024-08-18 21:02:33 -0300

    feat: Meeting room

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,19 @@
 import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
 import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
 
 interface RootLayoutType {
   children: ReactNode;
 }
+
 
 const RootLayout = ({ children }: RootLayoutType) => {
   return (
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -32,21 +32,24 @@ const MeetingSetup = ({
   }, [isMicCamToggledOn, call?.camera, call?.microphone]);
 
   return (
-    <div className="fllex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
       <h1 className="text-2xl font-bold">Setup</h1>
       <VideoPreview />
-      <div className="flex h-16 items-center justify-center gap-3">
-        <label className="flex items-center justify-center gap-2 font-medium">
-          <input
-            type="checkbox"
-            checked={isMicCamToggledOn}
-            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
-          />
-          Join with mic and camera off
-        </label>
-        <DeviceSettings />
+      <div className="flex flex-col h-16 items-center justify-center gap-3">
+        <div className="flex items-center gap-3 mt-6">
+          <label className="flex items-center justify-center gap-2 font-medium">
+            <input
+              type="checkbox"
+              checked={isMicCamToggledOn}
+              onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+            />
+            Join with mic and camera off
+          </label>
+          <DeviceSettings />
+        </div>
+
         <Button
-          className="rounded-md bg-gree-500 px-4 py-2.5"
+          className="rounded-md bg-green-500 px-4 py-2.5"
           onClick={() => {
             call.join();
 
